0986c8cb0b9e177e4a47275de136ce8650de0515
Simon Joseph <simonjoseph750@gmail.com>
2022-08-28 22:40:43 +0100

    remove generated source maps

--- src/domain/pojo/bank-creation.pojo.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.BankCreationPojo = void 0;
-var BankCreationPojo = /** @class */ (function () {
-    function BankCreationPojo() {
-    }
-    return BankCreationPojo;
-}());
-exports.BankCreationPojo = BankCreationPojo;
--- src/models/scrape-data.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.ScrapeData = void 0;
-var ScrapeData = /** @class */ (function () {
-    function ScrapeData() {
-    }
-    return ScrapeData;
-}());
-exports.ScrapeData = ScrapeData;
